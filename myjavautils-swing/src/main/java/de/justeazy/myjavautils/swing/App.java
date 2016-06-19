4c212c4be4f19dd3b1298684e89840ef1e002433
Henrik Peters <henrik.peters@tu-clausthal.de>
2016-06-19 21:41:25 +0200

    Added swing module

---
+++ myjavautils-swing/src/main/java/de/justeazy/myjavautils/swing/App.java
@@ -0,0 +1,13 @@
+package de.justeazy.myjavautils.swing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
