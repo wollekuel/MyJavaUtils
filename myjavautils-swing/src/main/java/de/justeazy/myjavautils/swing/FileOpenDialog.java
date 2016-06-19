6342489b7c265973c891dc66a902264b5077a9e0
Henrik Peters <henrik.peters@tu-clausthal.de>
2016-06-19 21:43:29 +0200

    Added a first class to swing module

---
+++ myjavautils-swing/src/main/java/de/justeazy/myjavautils/swing/FileOpenDialog.java
@@ -0,0 +1,12 @@
+package de.justeazy.myjavautils.swing;
+
+/**
+ * <p>
+ * Place holder class for future implementations.
+ * </p>
+ * 
+ * @author Henrik Peters
+ */
+public class FileOpenDialog {
+
+}
